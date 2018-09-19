f91fc0b00186851f9fd9cdd2a8d31440df2c08f3
Duch003 <tomaszmankin003@gmail.com>
2018-09-19 20:50:31 +0100

    The begining of creating SportStore -  ASP.NET Learning

---
+++ SportStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Policy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+namespace SportStore.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
